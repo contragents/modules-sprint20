d96176bc0a78d9ba4cfcaa8bef133ca9446222a0
contragents <32774489+contragents@users.noreply.github.com>
2017-10-15 12:01:18 +0300

    27

--- Quest62/hello.php
+++ Quest62/hello.php
@@ -1,13 +1,13 @@
 <?
 print_r($_SERVER);
 print_r($_GET);
-$data=file_get_contents("http://openapi.clearspending.ru/restapi/v3/contracts/select/?okdp_okpd={$_GET[okpd]}");
+print $data=file_get_contents("http://openapi.clearspending.ru/restapi/v3/contracts/select/?okdp_okpd={$_GET[okpd]}");
 $data=json_decode($data,true);
 $total=$data[contracts][total];
 foreach($data[contracts][data] as $contract)
-printr($contract[productsCountries]);
+print_r($contract[productsCountries]);
 print_r($data);
-print_r($dtat);
+
 exit();
 print date('d.m.Y'); 
 print 'sprint20'; 
