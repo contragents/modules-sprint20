e2ff7b578d689a87be64da0df0c06246e7163371
contragents <32774489+contragents@users.noreply.github.com>
2017-10-15 11:06:12 +0300

    10
    10

--- Quest62/hello.php
+++ Quest62/hello.php
@@ -1,7 +1,7 @@
 <?
 $data=file_get_contets("http://openapi.clearspending.ru/restapi/v3/contracts/select/?okpd=0112000");
 $data=json_decode($data,true);
-print_r($dtat)
+print_r($dtat);
 exit();
 print date('d.m.Y'); 
 print 'sprint20'; 
